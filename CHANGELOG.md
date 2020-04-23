a98b812a5f21ba90db5ca0321105a5d90f42919f
Thomas Tastet <31560900+ThomasT404@users.noreply.github.com>
2020-04-23 21:11:24 +0200

    0.3.0
    - add : support element in element (V2Block Engine)
    - bug fixed : @import was not included in the compilation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.3.0
+- add : support element in element (V2Block Engine)
+- bug fixed : @import was not included in the compilation
+
 ## 0.2.0
 - add : error is displayed if variable is not found
 - edit : if the input contains an error, the output will be an array of strings
--- bracketcss.js
+++ bracketcss.js
@@ -1,14 +1,20 @@
+let globalChild = {};
+
 function bracketcss(code) {
+    let ableToImport = true;
     let errors = [];
     let variables = {};
+
     let transpiled_css = "";
 
     let lines = code.split("\n").map(line => line.trim());
 
     let mediaQuery = false;
+    let mediaQueryBracketLevel = 0;
     let cssQuery = [];
     let breakPoints = [];
     let typeBreak = "max";
+    let cssBlock = [];
 
     let brackets = 0;
 
@@ -33,38 +39,50 @@ function bracketcss(code) {
             } else {
                 return variables[tmp[1]];
             }
-        })
+        });
 
-        if(mediaQuery) {
-            cssQuery.push(line);
-            if(line.includes("{")) {
-                brackets++;
-            }
+        if(line.includes("{")) {
+            ableToImport = false;
+            brackets++;
+        } else if(line.includes("}")){
+            brackets--;
+        }
 
-            if(line.includes("}")) {
-                brackets--;
-            }
-
-            if(brackets === 0) {
-                transpiled_css += block(cssQuery, breakPoints, typeBreak);
-
-                cssQuery = [];
-                breakPoints = [];
-
-                mediaQuery = false;
-            }
+        if(ableToImport && line[0] === "@") {
+            transpiled_css += line;
         } else {
-            if(line.toLowerCase().includes("@media screen and")) {
-                const tmp = /@media screen and \(?(([a-zA-Z0-9-])+):( )?\[(([a-zA-Z0-9, ])+)\]/.exec(line.toLowerCase());
+            if(mediaQuery) {
+                cssQuery.push(line);
 
-                typeBreak = tmp[1];
-                breakPoints = tmp[4].replace(/ /g, "").split(",");
+                if(brackets === mediaQueryBracketLevel) {
+                    transpiled_css += v2Block(cssQuery, breakPoints, typeBreak);
 
-                brackets = 1;
-                
-                mediaQuery = true;
+                    cssQuery = [];
+                    breakPoints = [];
+
+                    mediaQuery = false;
+                }
             } else {
-                transpiled_css += "\n" + line;
+                if(line.toLowerCase().includes("@media screen and")) {
+                    mediaQueryBracketLevel = brackets;
+                    const tmp = /@media screen and \(?(([a-zA-Z0-9-])+):( )?\[(([a-zA-Z0-9, ])+)\]/.exec(line.toLowerCase());
+
+                    typeBreak = tmp[1];
+                    breakPoints = tmp[4].replace(/ /g, "").split(",");
+
+                    mediaQuery = true;
+                } else {
+                    cssBlock.push(line);
+
+                    if(brackets === 0) {
+                        if(cssBlock.filter(line => line.length).length){
+                            transpiled_css += make(wrapperBlock(cssBlock));
+                            transpiled_css += makeGlobalChild();
+                        }
+
+                        cssBlock = [];
+                    }
+                }
             }
         }
     }
@@ -81,6 +99,121 @@ function bracketcss(code) {
     return errors.length ? errors : transpiled_css;
 }
 
+function makeChild(child, prefix = null, withBlocks = false) {
+    if(withBlocks){
+        return Object.keys(child).map(key => {
+            if(Object.keys(child[key].children).length) {
+                globalChild[`${prefix ? prefix + " "+ key : key}`] = child[key];
+                return makeChild(child[key].children, prefix ? prefix + " "+ key : key, withBlocks);
+            } else {
+                return [`${prefix ? prefix + " "+ key : key}{`,...child[key].code,`}`];
+            }
+        });
+    } else {
+        return Object.keys(child).map(key => {
+            if(Object.keys(child[key].children).length) {
+                globalChild[`${prefix ? prefix + " "+ key : key}`] = child[key];
+                return makeChild(child[key].children, prefix ? prefix + " "+ key : key, withBlocks);
+            } else {
+                return `${prefix ? prefix + " "+ key : key}{${child[key].code.join("")}}`;
+            }
+        });
+    }
+}
+function makeGlobalChild() {
+    let tmp = [];
+    Object.keys(globalChild).forEach(key => {
+        globalChild[key].children = {};
+
+        tmp = [...tmp, ...makeChild(globalChild, null, true)];
+    });
+
+    tmp = tmp.flat(Infinity);
+
+    globalChild = {};
+
+    return tmp.join("");
+}
+
+function make(object, withBlocks = false) {
+    let ref = object.main;
+
+    let result = null;
+
+    if(withBlocks) {
+        result = makeChild(ref.children, null, withBlocks).flat(Infinity);
+    } else {
+        result = makeChild(ref.children, null, withBlocks).flat(Infinity).join("");
+    }
+
+    return result;
+}
+
+function wrapperBlock(code) {
+    let style = {
+        main: {
+            children: {}
+        }
+    };
+    let cssPath = [];
+
+    let refCode = null;
+
+    for(let n = 0; n < code.length; n++) {
+        let line = code[n];
+
+        if (line[line.length - 1] === "{") {
+            let tmp = line.substr(0, line.length-1).trim();
+            cssPath.push(tmp);
+            let exist = null;
+            let el = style.main;
+            cssPath.forEach((element, index) => {
+                if(element[0] === "&" && index) {
+                    console.warn("& element is undefined");
+                } else {
+                    exist = el.children[element];
+                    if(!exist) {
+                        el.children[element] = {
+                            children: {},
+                            code: []
+                        }
+                    }
+
+                    refCode = el.children[element].code;
+
+                    el = el.children[element];
+                }
+            })
+        } else if(line[line.length - 1] === "}"){
+            cssPath.splice(cssPath.length - 1, 1);
+        } else {
+            if(refCode && line.length) {
+                refCode.push(line);
+            }
+        }
+    }
+
+    return style;
+}
+
+function v2Block(code = [], breakpoints = [], type = "max") {
+    // ---> first the global treatment with the new method
+    let firstStep = make(wrapperBlock(code), true);
+
+    Object.keys(globalChild).forEach(key => {
+        globalChild[key].children = {};
+
+        firstStep = [...firstStep, ...makeChild(globalChild, null, true)];
+    });
+
+    firstStep = firstStep.flat(Infinity);
+
+    globalChild = {};
+
+    // ---> then block processing using the old method
+    return block(firstStep, breakpoints, type);
+}
+
 //The block function is used to make the media queries using bracketcss
 function block(code = [], breakpoints = [], type = "max") {
     let style = {};
@@ -90,7 +223,7 @@ function block(code = [], breakpoints = [], type = "max") {
     let objectData = null
 
     for(let n = 0; n < code.length; n++) {
-        let line = code[n];
+        let line = code[n].trim();
         
         if(line[line.length - 1] === "{") {
             objectData = line.substr(0, line.length-1).trim();
@@ -101,12 +234,14 @@ function block(code = [], breakpoints = [], type = "max") {
             let property = /(.*):( )?(.*)/.exec(line.trim());
 
             if(property[3][0] !== "[") {
-                breakpoints.forEach(bp => style[bp][objectData].push(line));
+                breakpoints.forEach(bp => style[bp][objectData].includes(line) ? "" : style[bp][objectData].push(line));
             } else {
                 let values = property[3].substr(1)
                 values = values.substr(0, values.includes(";") ? values.length - 2 : values.length - 1).split(",").map(value => value.trim());
                 values.forEach((value, index) => {
-                    style[eq[index]][objectData].push(`${property[1]}: ${value};`);
+                    if(!style[eq[index]][objectData].includes(`${property[1]}: ${value};`)){
+                        style[eq[index]][objectData].push(`${property[1]}: ${value};`);
+                    }
                 })
             }
         }
@@ -118,7 +253,7 @@ function block(code = [], breakpoints = [], type = "max") {
 
     breakpoints.forEach(bp => {
         style_css += `@media screen and (${type}: ${bp}){`;
-        style_css += `${Object.keys(style[bp]).map(object => `${object}{${style[bp][object].join("")}}`)}`;
+        style_css += `${Object.keys(style[bp]).map(object => `${object}{${style[bp][object].join("")}}`).join("")}`;
         style_css += `}`;
     });
 
